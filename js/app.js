b950679d6b35f726999ef5ddfc8151333f8e3f33
Yelitza3125 <deyel_99@hotmail.com>
2017-12-10 02:53:13 -0500

    Creando la función showMenu en javascript

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 window.addEventListener('load', function() {
      var btn = document.getElementById('btn');
-     var menu = document.getElementsByTagName('menu');
+     var menu = document.getElementById('menu');
+     
+     
 
-    btn.addEventListener('click', function hideMenu(event) {
-        menu.style.display = 'none';
-    })
+    btn.addEventListener('click', function showMenu(event) {
+        var list = document.getElementById('list');
+        var title = document.getElementById('header');
+        menu.classList.toggle('menu-modal');
+        list.classList.add('list');
+        title.classList.add('title');
+
+    })    
 });
